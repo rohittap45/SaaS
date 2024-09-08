cf026527743230736200286b0ae4d5925773d961
Rohit Tap <rohittap2017@gmail.com>
2024-09-08 23:47:37 +0530

    changes in env variables

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2_py&2t!e*o8uwg#ep$-x$jf!cr22knc1q_=e85$4_ni^bu+vx'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DJANGO_DEBUG', cast=bool)
+
 
 ALLOWED_HOSTS = [
     ".railway.app",
